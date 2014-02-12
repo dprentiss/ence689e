94f8381b74441aedc5faf70162af7a65805ab9bb
dprentiss <dprentiss@gmail.com>
2014-02-12 14:48:12 -0500

    ps1 clean up

--- README.md
+++
@@ -1,6 +0,0 @@
-ence689e
-========
-
-ENCE689E Spring 2014
-
-MATLAB and LaTex files for class.
--- ps1/prentissPS1.m
+++ ps1/prentissPS1.m
@@ -1,5 +1,12 @@
-
 SWE_sum = 0;
 for i = [2:744]
     SWE_sum = SWE_sum + SWE_maps(:,:,i) - SWE_maps(:,:,i-1);
 end
+% generate labels from lat_deg and lon_deg
+xTL = lon_deg(1:20:151);
+xT = linspace(1, 151, numel(xTL));
+set(gca, 'XTick', xT, 'XTickLabel', xTL);
+
+yTL = lat_deg(1:20:151);
+yT = linspace(1, 151, numel(yTL));
+set(gca, 'YTick', yT, 'YTickLabel', yTL);
