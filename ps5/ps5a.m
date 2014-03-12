00176defe7b2e914bf2ee52c26ed8179491cdffd
David Prentiss <dprentiss@gmail.com>
2014-03-12 00:04:52 -0400

    ps5

--- ps5/ps5a.m
+++ ps5/ps5a.m
@@ -3,26 +3,50 @@ G = [1; 0];
 u = [0 0 70 70 0 0 0 0 0 0];
 T = 0:10;
 
-y_a = cell(1,11);
-y_a{1} = [50; 75];
-y_b = cell(1,11);
-y_b{1}= [70; 80];
+y_true = zeros(2,11);
+y_true(:,1) = [50; 75];
+y_b = zeros(2,11);
+y_b(:,1)= [70; 80];
 
 for i = 1:10
-    y_a{i+1} = A*y_a{i} + G*u(i);
-    y_b{i+1} = A*y_b{i} + G*u(i);
+    y_true(:,i+1) = A*y_true(:,i) + G*u(i);
+    y_b(:,i+1) = A*y_b(:,i) + G*u(i);
 end
 
-y_a_mat = cell2mat(y_a);
-y_true_1 = y_a_mat(1,:);
-y_true_2 = y_a_mat(2,:);
+plot(T, y_true(1,:), '-sk', T, y_true(2,:), '-ok', ...
+    T, y_b(1,:), ':sk', T, y_b(2,:), ':ok')
 
-y_b_mat = cell2mat(y_b);
-y_b_1 = y_b_mat(1,:);
-y_b_2 = y_b_mat(2,:);
+y1bias = fct_bias(y_b(1,:),y_true(1,:))
+y2bias = fct_bias(y_b(2,:),y_true(2,:))
+y1RMSE = fct_RMSE(y_b(1,:),y_true(1,:))
+y2RMSE = fct_RMSE(y_b(2,:),y_true(2,:))
 
-plot(T, y_true_1, '-sk', T, y_true_2, '-ok', ...
-    T, y_b_1, ':sk', T, y_b_2, ':ok')
+Cyy_c(:,:,1) = [400, 0; 0, 400];
 
-fct_bias(y_b_1,y_true_1)
-fct_RMSE(y_b_1,y_true_1)
+for i = 1:10
+    Cyy_c(:,:,i+1) = A*Cyy_c(:,:,i)*A';
+end
+
+yb1_up = y_b(1,:)' + 2*sqrt(squeeze(Cyy_c(1,1,:)));
+yb1_lw = y_b(1,:)' - 2*sqrt(squeeze(Cyy_c(1,1,:)));
+yb2_up = y_b(2,:)' + 2*sqrt(squeeze(Cyy_c(2,2,:)));
+yb2_lw = y_b(2,:)' - 2*sqrt(squeeze(Cyy_c(2,2,:)));
+
+plot(T, y_true(1,:), '-sk', T, y_true(2,:), '-ok', ...
+    T, y_b(1,:), '-.sk', T, y_b(2,:), '-.sk', ...
+    T, yb1_up, ':k', T, yb1_lw, ':k', ...
+    T, yb2_up, ':k', T, yb2_lw, ':k')
+
+rng(1);
+y_E = zeros(2,1000,11);
+y_E(:,:,1) = mvnrnd(y_b(:,1),Cyy_c(:,:,1),1000)';
+
+for i = 1:10
+    y_E(:,:,i+1) = A*y_E(:,:,i) + repmat(G*u(i),1,1000);
+end
+
+plot(T, y_true(1,:), '-k', T, y_true(2,:), '-k')
+hold on;
+for i = 1:100:1000
+     plot(T, squeeze(y_E(1,i,:)), ':k', T, squeeze(y_E(2,i,:)), ':k')
+end
