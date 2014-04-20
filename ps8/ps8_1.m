a7a7b7afa938be7f2101e8bf3c1bd06899fab0eb
David Prentiss <dprentiss@gmail.com>
2014-04-20 14:47:12 -0400

    some figures

---
+++ ps8/ps8_1.m
@@ -0,0 +1,22 @@
+ubar = [0 0 70 70 0 0 0 0 0 0];
+T = 0:9;
+plot(T,ubar)
+axis([0 9 -10 80])
+title('Nominal precipitation forcing')
+ylabel('mm water')
+xlabel('time step')
+
+rng(0);
+u = zeros(10,10);
+for i = 1:10
+    for j = 1:10
+        u(i,j) = ubar(j) + normrnd(0, 5);
+    end
+end
+
+figure
+plot(T,ubar)
+hold on
+for i = 1:10
+    plot(T,u(i,:))
+end
